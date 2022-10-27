4cf476754dd6aef13d384d265c53f18b13788e33
Jared Siemen <siemenjm@gmail.com>
2022-10-27 13:38:38 -0400

    Add nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <header>
+            // site logo
+            <nav>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/'}>About Me</Link>
+                <Link to={'/'}>Projects</Link>
+                <Link to={'/'}>Client Work</Link>
+                <Link to={'/'}>Contact</Link>
+            </nav>
+        </header>
     );
 }
