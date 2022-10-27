2e4920838fad2ffa5a7529d628b34ba1d192505d
Jared Siemen <siemenjm@gmail.com>
2022-10-27 13:09:39 -0400

    Add components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
-        
+        <Header />
+        <Main />
+        <Footer />
     </>
   );
 }
