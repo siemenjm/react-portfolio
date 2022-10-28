cf3ea1dafe3a1c782bb95879eb7bf5b24c139429
Jared Siemen <siemenjm@gmail.com>
2022-10-27 20:10:23 -0400

    Use react-responsive MediaQuery to conditionally render nav elements depending on screen size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import logo from '../assets/Color-logo-no-background.png';
@@ -10,14 +12,19 @@ export default function Header({ currentPage, setCurrentPage }) {
             <Link to={'/'} >
                 <img src={logo} alt="Jared Siemen website logo" className='site-logo' />
             </Link>
-            <nav>
-                <NavLink path={'/'} page={'Home'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                <NavLink path={'/about-me'} page={'About Me'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                <NavLink path={'/projects'} page={'Projects'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                <NavLink path={'/client-work'} page={'Client Work'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                <NavLink path={'/contact'} page={'Contact'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                <NavLink path={'/resume'} page={'Resume'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-            </nav>
+            <MediaQuery minWidth={1025} >
+                <nav className='desktop-nav'>
+                    <NavLink path={'/'} page={'Home'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                    <NavLink path={'/about-me'} page={'About Me'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                    <NavLink path={'/projects'} page={'Projects'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                    <NavLink path={'/client-work'} page={'Client Work'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                    <NavLink path={'/contact'} page={'Contact'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                    <NavLink path={'/resume'} page={'Resume'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                </nav>
+            </MediaQuery>
+            <MediaQuery maxWidth={1024} >
+                <FaBars />
+            </MediaQuery>
         </header>
     );
 }
