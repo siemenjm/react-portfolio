ac643f87366b808355d5d47a63f003c5b630e616
Jared Siemen <siemenjm@gmail.com>
2022-10-27 21:24:29 -0400

    Toggle nav dropdown menu in tablet mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,29 @@ import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import logo from '../assets/Color-logo-no-background.png';
 import NavLink from './NavLink';
+import { useState } from 'react';
 
 export default function Header({ currentPage, setCurrentPage }) {
+    const [dropdownVisibility, setDropdownVisibility] = useState(false);
+
+    function handleClick(e) {
+        console.log('clicked');
+        dropdownVisibility ? setDropdownVisibility(false) : setDropdownVisibility(true);
+    }
+
+    function displayDropdown() {
+        return (
+            <nav className='responsive-nav'>
+                <NavLink path={'/'} page={'Home'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                <NavLink path={'/about-me'} page={'About Me'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                <NavLink path={'/projects'} page={'Projects'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                <NavLink path={'/client-work'} page={'Client Work'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                <NavLink path={'/contact'} page={'Contact'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                <NavLink path={'/resume'} page={'Resume'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+            </nav>
+        );
+    }
+
     return (
         <header>
             <Link to={'/'} >
@@ -23,7 +44,8 @@ export default function Header({ currentPage, setCurrentPage }) {
                 </nav>
             </MediaQuery>
             <MediaQuery maxWidth={1024} >
-                <FaBars />
+                <FaBars onClick={handleClick} />
+                {dropdownVisibility ? displayDropdown() : <></>}
             </MediaQuery>
         </header>
     );
