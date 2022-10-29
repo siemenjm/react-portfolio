e04fb500baa124c34c89151002177d25ad71be02
Jared Siemen <siemenjm@gmail.com>
2022-10-29 15:07:06 -0400

    Remove comments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,20 +19,6 @@ export default function Footer() {
             <div className="footer-bottom">
                 <p>Copyright 2022 Jared Siemen, All Rights Reserved | Designed and Built by <a href='https://jaredsiemen.com'>Jared Siemen</a></p>
             </div>
-            {/* <MediaQuery minWidth={1025} >
-                <nav className='desktop-nav'>
-                    <NavLink path={'/'} page={'Home'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                    <NavLink path={'/about-me'} page={'About Me'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                    <NavLink path={'/projects'} page={'Projects'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                    <NavLink path={'/client-work'} page={'Client Work'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                    <NavLink path={'/contact'} page={'Contact'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                    <NavLink path={'/resume'} page={'Resume'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                </nav>
-            </MediaQuery>
-            <MediaQuery maxWidth={1024} >
-                {dropdownVisibility ? <FaRegWindowClose onClick={handleClick} /> : <FaBars onClick={handleClick} />}
-                {dropdownVisibility ? displayDropdown() : <></>}
-            </MediaQuery> */}
         </footer>
     );
 }
