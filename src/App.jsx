8e8c5c9b52efbd7ec6ed51eab0fa43770067324f
Jared Siemen <siemenjm@gmail.com>
2022-10-31 15:03:59 -0400

    Remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './styles/App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
-import { useState } from 'react';
 
 function App() {
     return (
