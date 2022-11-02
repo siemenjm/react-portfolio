8489d331061e7e4f79c7062de52dfb2ff371317e
Jared Siemen <siemenjm@gmail.com>
2022-11-02 15:53:32 -0400

    Add floating animation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/HeroSection.css';
+import '../styles/Section.css';
 import NavLink from '../components/NavLink';
 
 export default function HomePage({ currentPage, setCurrentPage }) {
@@ -19,6 +20,17 @@ export default function HomePage({ currentPage, setCurrentPage }) {
                 <p>I’m a full stack software developer with a diverse skillset that ranges from building single page applications using React and Express.js, to quickly churning out a marketing site for a small business, to designing a website interface from the ground up using Adobe Xd. Currently freelancing, but looking to join a team.</p>
                 <div className="button-wrapper">
                     <NavLink path={'/contact'} pageTitle={'Let\'s Connect'} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                </div>
+            </section>
+            <section className="about-me-section">
+            <div className="section-header-wrapper">
+                    <h2 className={`header-letter first-letter color-accent`}>A</h2>
+                    <h2 className='header-letter second-letter color-accent'>b</h2>
+                    <h2 className='header-letter third-letter color-accent'>o</h2>
+                    <h2 className='header-letter fourth-letter color-accent'>u</h2>
+                    <h2 className='header-letter fifth-letter color-accent'>t</h2>
+                    <h2 className='header-letter sixth-letter color-accent'>M</h2>
+                    <h2 className='header-letter seventh-letter color-accent'>e</h2>
                 </div>
             </section>
         </div>
