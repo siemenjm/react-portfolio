8c5cbb081b358f3254ec377dc5eb7e26e5e98403
Jared Siemen <siemenjm@gmail.com>
2022-11-05 12:50:00 -0400

    Move testimonial data into data file

---
+++ src/data/testimonial_data.js
@@ -0,0 +1,13 @@
+import allSportsPhotographyLogo from '../assets/All_Sports_Photography_logo.png';
+
+const testimonialData = [
+    {
+        author: 'Don Montague',
+        authorBusiness: 'All Sports Photography',
+        authorImage: allSportsPhotographyLogo,
+        testimonial: '"Jared delivered amazing results on the project and I enjoyed working with him. His communication was top-notch, he met all deadlines, and his skills were excellent. I enjoyed working with Jared and will likely have additional projects for him in the future."',
+        deployedLink: 'https://allsportsphotography-fl.com/',
+    },
+];
+
+export default testimonialData;
