8ee9d2de0f17ed2ee48702c1c9eba3ea95883519
Jared Siemen <siemenjm@gmail.com>
2022-11-06 13:19:44 -0500

    Add left and right buttons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import '../styles/Carousel.css';
 import testimonialData from '../data/testimonial_data';
 import Testimonial from '../components/Testimonial';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 export default function Carousel() {
     const allTestimonials = testimonialData.map((testimonial, index) => {
@@ -9,9 +11,9 @@ export default function Carousel() {
 
     return (
         <div className='carousel'>
-            <button className="carousel-button"></button>
+            <button className="carousel-button"><FaChevronLeft /></button>
             {allTestimonials}
-            <button className="carousel-button"></button>
+            <button className="carousel-button"><FaChevronRight /></button>
         </div>
     );
 }
