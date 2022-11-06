c1092e854c3b6cc4fef979a861a7e0a36524b1fb
Jared Siemen <siemenjm@gmail.com>
2022-11-06 13:31:36 -0500

    Cycle through testimonials using arrows

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Carousel.css';
 import testimonialData from '../data/testimonial_data';
 import Testimonial from '../components/Testimonial';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 export default function Carousel() {
-    const allTestimonials = testimonialData.map((testimonial, index) => {
-        return <Testimonial testimonial={testimonial} key={`${testimonial.author}-${index}`} />
-    });
+    const [currentTest, setCurrentTest] = useState(0);
+
+    function handleLeftClick(e) {
+        if (currentTest === 0) {
+            setCurrentTest(testimonialData.length - 1);
+        } else {
+            setCurrentTest(currentTest - 1);
+        }
+    }
+
+    function handleRightClick(e) {
+        if (currentTest === testimonialData.length - 1) {
+            setCurrentTest(0);
+        } else {
+            setCurrentTest(currentTest + 1);
+        }
+    }
+
+    // const allTestimonials = testimonialData.map((testimonial, index) => {
+    //     return <Testimonial testimonial={testimonial} key={`${testimonial.author}-${index}`} />
+    // });
 
     return (
         <div className='carousel'>
-            <button className="carousel-button"><FaChevronLeft /></button>
-            {allTestimonials}
-            <button className="carousel-button"><FaChevronRight /></button>
+            <button onClick={handleLeftClick} className="carousel-button"><FaChevronLeft /></button>
+            <Testimonial testimonial={testimonialData[currentTest]} />
+            <button onClick={handleRightClick} className="carousel-button"><FaChevronRight /></button>
         </div>
     );
 }
