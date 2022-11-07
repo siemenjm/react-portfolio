78a2fc511c69c7687eee3e77976bc42530df89db
Jared Siemen <siemenjm@gmail.com>
2022-11-07 11:27:19 -0500

    Added CSS animation to scroll through testimonials. Removed buttons.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,29 +5,17 @@ import Testimonial from '../components/Testimonial';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 export default function Carousel() {
-    const [currentTest, setCurrentTest] = useState(0);
-
-    function handleLeftClick(e) {
-        if (currentTest === 0) {
-            setCurrentTest(testimonialData.length - 1);
-        } else {
-            setCurrentTest(currentTest - 1);
-        }
-    }
-
-    function handleRightClick(e) {
-        if (currentTest === testimonialData.length - 1) {
-            setCurrentTest(0);
-        } else {
-            setCurrentTest(currentTest + 1);
-        }
-    }
+    const allTestimonials = testimonialData.map((testimonial) => {
+        return <Testimonial testimonial={testimonial} />
+    });
 
     return (
         <div className='carousel'>
-            <button onClick={handleLeftClick} className="carousel-button"><FaChevronLeft /></button>
-            <Testimonial testimonial={testimonialData[currentTest]} />
-            <button onClick={handleRightClick} className="carousel-button"><FaChevronRight /></button>
+            {/* <button className="carousel-button"><FaChevronLeft /></button> */}
+            <div className="testimonial-container">
+                {allTestimonials}
+            </div>
+            {/* <button className="carousel-button"><FaChevronRight /></button> */}
         </div>
     );
 }
