ec2323a5f3e2274437aa7472edf3555ca5df0606
Jared Siemen <siemenjm@gmail.com>
2022-11-10 15:11:11 -0500

    Populate contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function ContactPage() {
     return (
-        <div className='page'>ContactPage</div>
+        <div className='page'>
+            <section className="contact-section single-page">
+                <div className="line-effects">
+                    <div className="line-effect-container horizontal">
+                        <div className="horizontal-line-effect"></div>
+                        <h2 className="line-header color-accent">Say Hi!</h2>
+                    </div>
+                    <div className="line-effect-container vertical">
+                        <div className="vertical-line-effect"></div>
+                    </div>
+                </div>
+                <div className="content-wrapper left">
+                    <h3>Like what you see? Reach out and let's connect!</h3>
+                    <p>Send me an email at <span>contact@jaredsiemen.com</span> or reach out to me on <a href='https://www.linkedin.com/in/jaredsiemen' target='_blank' rel='noopener noreferrer' >LinkedIn</a>. You can also check out my Github repos by clickint the icon below. I'm looking forward to hearing from you!</p>
+                    <div className="link-wrapper">
+                        <FaGithub className='project-link-icon' />
+                        <FaLinkedin className='project-link-icon' />
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 }
