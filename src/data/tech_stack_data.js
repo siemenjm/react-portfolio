3a3d330e5a271c3acbc98a594c28d7b143f5d6c8
Jared Siemen <siemenjm@gmail.com>
2022-11-10 14:28:50 -0500

    Add tech stack data file

---
+++ src/data/tech_stack_data.js
@@ -0,0 +1,19 @@
+const techStackData = [
+    {
+        title: 'Thrift',
+        shortDesc: 'Financial Tracking App',
+        screenshot: thriftScreenshot,
+        screenshotAlt: 'Thrift screenshot showing a financial graph',
+        longDesc: 'Thrift is a money-tracking web application built using the PERN stack. Users can track their transactions, accounts, and the institutions that they belong to. Some basic charts are also implemented using Chart.js.',
+        projectTech: [
+            'PostgreSQL',
+            'Express.js',
+            'React.js',
+            'Node.js'
+        ],
+        githubLink: 'https://github.com/siemenjm/backend-thrift',
+        deployedLink: 'https://thrift-financial.netlify.app/',
+    },
+];
+
+export default techStackData;
