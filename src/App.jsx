86128638bd63e4d1f803c3313399a1762b8117ba
Jared Siemen <siemenjm@gmail.com>
2022-11-22 15:27:22 -0500

    Use useLocation hook to set currentPage state based on pathname in URL to fix style bug when reloading

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './styles/App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import { useLocation } from 'react-router';
 
 function App() {
-    const [currentPage, setCurrentPage] = useState('/');
+    const location = useLocation();
+
+    const [currentPage, setCurrentPage] = useState(location.pathname);
 
     return (
         <>
