bd304da70f013e06c29a85bb9b344532ffd9eeee
Jared Siemen <siemenjm@gmail.com>
2022-11-22 15:33:38 -0500

    Add console.log to debug live version

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { useLocation } from 'react-router';
 
 function App() {
     const location = useLocation();
+    console.log(location)
 
     const [currentPage, setCurrentPage] = useState(location.pathname);
 
