a0143713aab976991d160f393d7d3a9498437f84
Jared Siemen <siemenjm@gmail.com>
2022-12-20 20:55:43 -0500

    Remove GA code from index.js and add to index.html

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-import ReactGA from 'react-ga';
-const TRACKING_ID = 'UA-217570358-1';
-ReactGA.initialize(TRACKING_ID);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
