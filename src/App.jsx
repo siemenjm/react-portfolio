303e00101bbce1d9e5222419dfcb78075264b2fb
Jared Siemen <siemenjm@gmail.com>
2022-12-20 20:48:52 -0500

    Move GA code from app to index

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ import Header from './components/Header';
 import Main from './components/Main';
 import { useLocation } from 'react-router';
 
-import ReactGA from 'react-ga';
-const TRACKING_ID = 'UA-217570358-1';
-ReactGA.initialize(TRACKING_ID);
-
 function App() {
   const location = useLocation();
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import ReactGA from 'react-ga';
+const TRACKING_ID = 'UA-217570358-1';
+ReactGA.initialize(TRACKING_ID);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
