250c23018a1d959d46238eb873544f2a2db88930
Jared Siemen <siemenjm@gmail.com>
2022-12-20 20:30:40 -0500

    Add NextJS, Tailwind, TypeScript to tech stack data

--- src/data/tech_stack_data.js
+++ src/data/tech_stack_data.js
@@ -7,89 +7,107 @@ import githubLogo from '../assets/GitHub_logo.png';
 import htmlLogo from '../assets/HTML_logo.png';
 import jsLogo from '../assets/JavaScript-logo.png';
 import mongoLogo from '../assets/MongoDB_Logo.svg';
+import nextjsLogo from '../assets/NextJS_logo.png';
 import nodeLogo from '../assets/Node.js_logo.svg';
 import phpLogo from '../assets/php_logo.png';
 import postgresqlLogo from '../assets/PostgreSQL_logo.svg';
 import pythonLogo from '../assets/python-logo-only.png';
 import reactLogo from '../assets/React-icon.svg';
+import tailwindLogo from '../assets/Tailwind_logo.svg';
+import tsLogo from '../assets/TypeScript_logo.png';
 import wordpressLogo from '../assets/WordPress_logo.svg';
 
 const techStackData = [
-    {
-        tech: 'HTML',
-        logo: htmlLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'CSS',
-        logo: cssLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'Bootstrap',
-        logo: bootstrapLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'JavaScript',
-        logo: jsLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'Python',
-        logo: pythonLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'React.js',
-        logo: reactLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'Node.js',
-        logo: nodeLogo,
-        lightBackground: true,
-    },
-    {
-        tech: 'Express.js',
-        logo: expressLogo,
-        lightBackground: true,
-    },
-    {
-        tech: 'MongoDB',
-        logo: mongoLogo,
-        lightBackground: true,
-    },
-    {
-        tech: 'PostgreSQL',
-        logo: postgresqlLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'PHP',
-        logo: phpLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'WordPress',
-        logo: wordpressLogo,
-        lightBackground: true,
-    },
-    {
-        tech: 'Elementor',
-        logo: elementorLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'Adobe Xd',
-        logo: adobeXdLogo,
-        lightBackground: false,
-    },
-    {
-        tech: 'GitHub',
-        logo: githubLogo,
-        lightBackground: false,
-    },
+  {
+    tech: 'HTML',
+    logo: htmlLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'CSS',
+    logo: cssLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'JavaScript',
+    logo: jsLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'TypeScript',
+    logo: tsLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'React.js',
+    logo: reactLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'Next.js',
+    logo: nextjsLogo,
+    lightBackground: true,
+  },
+  {
+    tech: 'Node.js',
+    logo: nodeLogo,
+    lightBackground: true,
+  },
+  {
+    tech: 'Express.js',
+    logo: expressLogo,
+    lightBackground: true,
+  },
+  {
+    tech: 'MongoDB',
+    logo: mongoLogo,
+    lightBackground: true,
+  },
+  {
+    tech: 'PostgreSQL',
+    logo: postgresqlLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'Bootstrap',
+    logo: bootstrapLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'Tailwind',
+    logo: tailwindLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'Python',
+    logo: pythonLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'PHP',
+    logo: phpLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'GitHub',
+    logo: githubLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'WordPress',
+    logo: wordpressLogo,
+    lightBackground: true,
+  },
+  {
+    tech: 'Elementor',
+    logo: elementorLogo,
+    lightBackground: false,
+  },
+  {
+    tech: 'Adobe Xd',
+    logo: adobeXdLogo,
+    lightBackground: false,
+  },
 ];
 
 export default techStackData;
